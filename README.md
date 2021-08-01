6a0788a4a80a34ac010eee1d60368189ddd53054
Ilay Bokobza <kanoel200@gmail.com>
2021-08-01 14:30:38 +0300

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+### Console Cam
+Allows you to see your webcam in the console.
+
+### Setup
+First copy the project to your computer.
+then run:
+```bash
+pip install yachalk Pillow opencv-python
+```
+or if you have nodejs:
+```bash
+npm run get
+```
+
+#
+Now just run the file!
---
+++ main.py
@@ -0,0 +1,36 @@
+from io import BytesIO
+from PIL import Image
+from colorama import Fore
+from yachalk import chalk
+import cv2
+
+def processImage(data):
+    ySize = 75
+    xSize = 150
+
+    image = Image.open(data)
+    image = image.resize((xSize,ySize))
+    image = image.transpose(Image.FLIP_LEFT_RIGHT)
+    pixels = image.load()
+    image.close()
+
+    output = ""
+    for y in range(ySize):
+        for x in range(xSize):
+            pixel = pixels[x,y]
+            output += chalk.rgb(pixel[0],pixel[1],pixel[2]).bold("#")
+
+        output += "\n"
+    
+    print(output)
+
+def main():
+    camera = cv2.VideoCapture(0)
+
+    while True:
+        _,frame = camera.read()
+        frameData = cv2.imencode('.webp',frame)[1].tobytes()
+        processImage(BytesIO(frameData))
+        
+
+main()
